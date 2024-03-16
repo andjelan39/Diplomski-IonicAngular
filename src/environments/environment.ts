78f6e5e67ccfd8271565879d1c5f480496d3d19c
Dragana <andjelaneskovic39@gmail.com>
2024-03-16 02:35:57 +0100

    Dovlacenje filmova i dodavanje Movie details strane

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'e2c7d61eab245566eab44e4c320ad150',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
